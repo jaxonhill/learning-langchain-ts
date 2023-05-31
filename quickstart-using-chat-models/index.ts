119e31d4bfc5a4d0471dc0a0c7a00b6ef25bcb84
jaxonhill <jaxoncharleshill@gmail.com>
2023-05-31 16:23:07 -0700

    Chains

--- quickstart-using-chat-models/index.ts
+++ quickstart-using-chat-models/index.ts
@@ -2,6 +2,7 @@ import { ChatOpenAI } from "langchain/chat_models/openai";
 import { HumanChatMessage, SystemChatMessage } from "langchain/schema";
 import { SystemMessagePromptTemplate, HumanMessagePromptTemplate, ChatMessagePromptTemplate, ChatPromptTemplate } from "langchain/prompts";
 import "dotenv/config";
+import { LLMChain } from "langchain/chains";
 
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
@@ -24,6 +25,34 @@ const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
 // MESSAGE TEMPLATES
 
+// const chat: ChatOpenAI = new ChatOpenAI({ openAIApiKey: OPENAI_API_KEY });
+
+// // Create an encompassing ChatPromptTemplate that is made up of other PromptTemplates
+// // In this case, a SystemMessagePromptTemplate and a HumanMessagePromptTemplate
+// const translationPromptTemplate = ChatPromptTemplate.fromPromptMessages([
+//     SystemMessagePromptTemplate.fromTemplate(
+//         "You are a helpful assistant that translates {input_language} to {output_language}."
+//     ),
+//     HumanMessagePromptTemplate.fromTemplate(
+//         "{text}"
+//     ),
+// ]);
+
+// // Pass in variables to create the final prompt object that contains an array of Messages
+// const finalPromptObject = await translationPromptTemplate.formatPromptValue({
+//     input_language: "English",
+//     output_language: "Spanish",
+//     text: "I love programming a computer",
+// });
+
+// const response = await chat.generatePrompt([finalPromptObject,]);
+
+// console.log(response.generations[0]);
+
+
+
+// Asking for the completion of a formatted prompt is pretty common, so chains make it easier
+
 const chat: ChatOpenAI = new ChatOpenAI({ openAIApiKey: OPENAI_API_KEY });
 
 // Create an encompassing ChatPromptTemplate that is made up of other PromptTemplates
@@ -37,13 +66,16 @@ const translationPromptTemplate = ChatPromptTemplate.fromPromptMessages([
     ),
 ]);
 
-// Pass in variables to create the final prompt object that contains an array of Messages
-const finalPromptObject = await translationPromptTemplate.formatPromptValue({
+const chain = new LLMChain({
+    prompt: translationPromptTemplate,
+    llm: chat,
+})
+
+// Pass variables right into the chain call, instead of formatting first, then creating separate call
+const response = await chain.call({
     input_language: "English",
     output_language: "Spanish",
-    text: "I love programming a computer",
+    text: "I love programming using Artificial Intelligence. I like speaking in Spanish, translate this also."
 });
 
-const response = await chat.generatePrompt([finalPromptObject,]);
-
-console.log(response.generations[0]);
+console.log(response);
