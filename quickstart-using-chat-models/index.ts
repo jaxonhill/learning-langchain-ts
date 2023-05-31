e4286fdf1746f23b43a140d94e0134c6aa727fab
jaxonhill <jaxoncharleshill@gmail.com>
2023-05-31 16:12:32 -0700

    Chat Prompt templates

--- quickstart-using-chat-models/index.ts
+++ quickstart-using-chat-models/index.ts
@@ -1,13 +1,13 @@
 import { ChatOpenAI } from "langchain/chat_models/openai";
 import { HumanChatMessage, SystemChatMessage } from "langchain/schema";
-import { SystemMessagePromptTemplate, HumanMessagePromptTemplate, ChatMessagePromptTemplate } from "langchain/prompts";
+import { SystemMessagePromptTemplate, HumanMessagePromptTemplate, ChatMessagePromptTemplate, ChatPromptTemplate } from "langchain/prompts";
 import "dotenv/config";
 
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
 // BASIC API CALLS
 
-// const chat: ChatOpenAI = new ChatOpenAI({ openAIApiKey: OPENAI_API_KEY })
+// const chat: ChatOpenAI = new ChatOpenAI({ openAIApiKey: OPENAI_API_KEY });
 
 // const response = await chat.call([
 //     new SystemChatMessage(
@@ -24,3 +24,26 @@ const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
 // MESSAGE TEMPLATES
 
+const chat: ChatOpenAI = new ChatOpenAI({ openAIApiKey: OPENAI_API_KEY });
+
+// Create an encompassing ChatPromptTemplate that is made up of other PromptTemplates
+// In this case, a SystemMessagePromptTemplate and a HumanMessagePromptTemplate
+const translationPromptTemplate = ChatPromptTemplate.fromPromptMessages([
+    SystemMessagePromptTemplate.fromTemplate(
+        "You are a helpful assistant that translates {input_language} to {output_language}."
+    ),
+    HumanMessagePromptTemplate.fromTemplate(
+        "{text}"
+    ),
+]);
+
+// Pass in variables to create the final prompt object that contains an array of Messages
+const finalPromptObject = await translationPromptTemplate.formatPromptValue({
+    input_language: "English",
+    output_language: "Spanish",
+    text: "I love programming a computer",
+});
+
+const response = await chat.generatePrompt([finalPromptObject,]);
+
+console.log(response.generations[0]);
